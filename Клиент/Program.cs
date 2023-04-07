35ce578347fd7041fbaeae8a8e45ebcca71ed9d2
shtrih2k4 <andreyzhevagin@gmail.com>
2023-04-07 22:45:41 +0300

    fixed

---
+++ Клиент/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+using System.Text;
+
+string[] paths=new string[1] { @"C:\Users\zevas\Desktop\1.jpeg"};
+using TcpClient client = new();
+await client.ConnectAsync("127.0.0.1", 8888);
+NetworkStream stream = client.GetStream();
+
+using StreamReader reader=new StreamReader(stream);
+using StreamWriter writer=new StreamWriter(stream);
+
+foreach(string path in paths)
+{
+
+    await writer.WriteLineAsync(path);
+    await writer.FlushAsync();
+    string? result = await reader.ReadLineAsync();
+    Console.WriteLine(result);
+}
+await writer.WriteLineAsync("END");
+await writer.FlushAsync();
---
+++ Отправка и получение фото через TCP/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+TcpListener server = new(IPAddress.Any, 8888);
+string uploadpath = @$"{Directory.GetCurrentDirectory()}\uploads";
+Directory.CreateDirectory(uploadpath);
+try
+{
+    server.Start();
+    Console.WriteLine("Server is loaded");
+    while (true)
+    {
+        TcpClient client = await server.AcceptTcpClientAsync();
+        await Task.Run(async () => await ProcessClient(client,uploadpath));
+    }
+}
+finally
+{
+    server.Stop();
+}
+async Task ProcessClient(TcpClient client,string uploadpath)
+{
+    NetworkStream stream = client.GetStream();
+    using StreamReader reader= new (stream);
+    using StreamWriter writer = new(stream);
+    while (true)
+    {
+        string? filepath = await reader.ReadLineAsync();
+        if (filepath == "END") break;
+        if (filepath is not null)
+        {
+            string? filename = filepath.Split(@"\")[^1];
+            string fullpath = $@"{uploadpath}\{filename}";
+            byte[] file_to_bytes = await File.ReadAllBytesAsync(filepath!);
+            using (FileStream newfile = new FileStream(fullpath, FileMode.Create))
+            {
+                await newfile.WriteAsync(file_to_bytes);
+                await newfile.FlushAsync();
+            }
+
+            await writer.WriteLineAsync($"{filepath} is downloaded");
+
+            await writer.FlushAsync();
+        }
+       
+    }
+    Console.WriteLine($"Files from {client.Client.RemoteEndPoint} are downloaded ");
+}
+
